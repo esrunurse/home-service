fab13e47cf009e596f293e4b2de4feb9721e1548
Kanjanapas <oshin_2544@hotmail.com>
2022-05-30 12:30:47 +0700

    created navbar

--- client/src/CustomerPhoto/imageIndex.js
+++ client/src/CustomerPhoto/imageIndex.js
@@ -1,11 +1,15 @@
 import orderIcon from './icons/order-icon.png'
 import paymentIcon from './icons/payment-icon.png'
 import penIcon from './icons/pen-icon.png'
+import personIcon from './icons/person-icon.png'
+import time from './icons/time.png'
 
 const image = {
   orderIcon,
   paymentIcon,
-  penIcon
+  penIcon,
+  personIcon,
+  time
 }
 
 export default image
